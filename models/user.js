edbfa08204f3d9a4070e6a23516a784b1c8b44c7
DickLan <pokemaster556610@gmail.com>
2023-07-21 10:52:32 +0800

    feat: add user registeration feature

--- models/user.js
+++ models/user.js
@@ -18,8 +18,11 @@ const userSchema = new Schema({
     default: Date.now
     // Date.now vs Date.now()
     // 沒():代表傳入的是函式本身
-    // 有():代表直接呼叫這個函式，並傳入函式的回傳值
+    // 這是mongoose lib做好的機制，當default是函式時 mongoose就會把他記住，然後在每次建立資料的當下，去執行並獲得預設值
     // 在js中 我們能將 函式當成物件屬性 所以可以把Date.now這函式當作createdAt的屬性，當伺服器將註冊資料送給資料庫時，才會執行Date.now並產生當下的時間戳記
+    // 這裡只是一個引用，意味著，當使用者使用mongoose.model創建user模型，並儲存新User時，才會被執行
+
+    // 有():代表直接呼叫這個函式，並傳入函式的回傳值
     // 若使用Date.now()則會在伺服器建立並運作的當下，就被執行
   }
 })
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -11,6 +12,40 @@ router.post('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+router.post('/register', (req, res) => {
+  // 取得註冊表單參數
+  const { name, email, password, confirmPassword } = req.body
+  // 檢查使用者是否已經註冊
+  User.findOne({ email }).then(user => {
+    // 如果已經註冊 就退回原本註冊畫面
+    if (user) {
+      console.log('already registered.')
+      res.render('register', {
+        name, email, password, confirmPassword
+      })
+    } else {
+      // 如果還沒註冊 就寫入資料庫
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+
+      // 也可以寫成
+      // const newUser = new User({
+      //   name,
+      //   email,
+      //   password
+      // })
+      // newUser.save()
+      //   .then(() => res.redirect('/'))
+      //   .catch(error => console.log(error))
+    }
+  })
+    .catch(error => console.log(error))
 })
 
 

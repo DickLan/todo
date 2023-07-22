5a7eb50e2c20bd78a65d01bd9b93fd1bf00cf7eb
DickLan <pokemaster556610@gmail.com>
2023-07-22 10:20:26 +0800

    feat: add user login via passport

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ const bodyParser = require('body-parser')
 // 載入method-module
 const methodOverride = require('method-override')
 
-require('./config/mongoose')
 // const COCK = require('./config/test')
+require('./config/mongoose')
+
+// 引用路由器
+// 引入路由器時 若路徑設定為/routes 就會自動去尋找目錄下名為index的檔案
+// 所以這裡其實是在 require routes底下的 index.js
+const routes = require('./routes')
+
+// 載入 passport 設定檔 必需要在express-session 之後
+// 定義 passport 函式
+const usePassport = require('./config/passport')
+
 // 設定app為express伺服器
 // 這個伺服器屬於應用程式 和資料庫伺服器不同
 const app = express()
@@ -23,10 +33,9 @@ app.use(methodOverride('_method'))
 //需要在網址使用query string "?" 例如?_method=xx 當路由設定了這組字串
 // 就會覆蓋html form預設的post方式
 
-// 引用路由器
-// 引入路由器時 若路徑設定為/routes 就會自動去尋找目錄下名為index的檔案
-// 所以這裡其實是在 require routes底下的 index.js
-const routes = require('./routes')
+// 調用 usePassport 函式 並傳入必要參數 app
+usePassport(app)
+
 // 將request導入
 app.use(routes)
 
---
+++ config/passport.js
@@ -0,0 +1,37 @@
+const passport = require('passport')
+// 需注意 LS 的載入方式 是官方的指定寫法 必續這樣寫 .Strategy
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user')
+// 這裡匯出的是一個 function
+// 在其他地方調用時 app 會是函示的參數
+module.exports = app => {
+  // 初始化 passport 模組
+  app.use(passport.initialize())
+  app.use(passport.session())
+  // 設定本地登入策略
+  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          return done(null, false, { message: 'Mail not registered!' })
+        }
+        if ((user.password !== password)) {
+          return done(null, false, { message: "Email or password incorrect." })
+        }
+        return done(null, user, { message: 'login success!' })
+      })
+      .catch(err => done(err, null))
+  }))
+
+  // 設定序列化與反序列化
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .lean()
+      .then(user => done(null, user))
+      .catch(err => done(err, null))
+  })
+
+}

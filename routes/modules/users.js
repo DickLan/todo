3208177c30f1e57faf3f247a55e93d609b0827f3
DickLan <pokemaster556610@gmail.com>
2023-07-26 11:42:57 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,19 +2,46 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
+
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-const loginSuccessMessage = (req, res, next) => {
-  req.flash('success_msg', '您已成功登入')
-  next()
-}
-router.post('/login', loginSuccessMessage, passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
+// const loginSuccessMessage = (req, res, next) => {
+//   req.flash('success_msg', '您已成功登入')
+//   next()
+// }
+// router.post('/login',
+//   passport.authenticate('local', {
+//     successRedirect: '/',
+//     failureRedirect: '/users/login'
+//   }))
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err)
+    }
+    // 當 passport.authenticate 驗證失敗的時候
+    // user 就會是 false
+    console.log(`req.body=${req.body}`)
+    console.log(`user=${user}`);
+    if (!user) {
+      req.flash('warning_msg', '無此使用者或密碼錯誤')
+      return res.redirect('/users/login')
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err)
+      }
+      req.flash('success_msg', '您已成功登入')
+      console.log(user);
+      return res.redirect('/')
+    })
+  })(req, res, next)
+})
+
 
 router.get('/logout', (req, res) => {
   req.logout() //passport.js提供的函式 會清除session
@@ -53,29 +80,40 @@ router.post('/register', (req, res) => {
       res.render('register', {
         errors, name, email, password, confirmPassword
       })
-    } else {
-      // 如果還沒註冊 就寫入資料庫
-      return User.create({
-        name,
-        email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error))
-
-      // 也可以寫成
-      // const newUser = new User({
-      //   name,
-      //   email,
-      //   password
-      // })
-      // newUser.save()
-      //   .then(() => res.redirect('/'))
-      //   .catch(error => console.log(error))
     }
+    // 如果還沒註冊 就寫入資料庫
+    return bcrypt
+      .genSalt(10) // generate sale with k 10
+      .then(salt => {
+        console.log(salt)
+        return bcrypt.hash(password, salt)
+      }) // 為使用者密碼加鹽，產生雜湊值
+      .then(hash => {
+        console.log(hash)
+        return User.create({
+          name,
+          email,
+          password
+        })
+      })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+
+
   })
     .catch(error => console.log(error))
 })
 
+
+// return bcrypt
+// .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+// .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+// .then(hash => User.create({
+//   name,
+//   email,
+//   password: hash // 用雜湊值取代原本的使用者密碼
+// }))
+// .then(() => res.redirect('/'))
+// .catch(err => console.log(err))
 
 module.exports = router
